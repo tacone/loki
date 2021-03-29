3c59fd481c71d96d3d5eaec844582a7fb52b1a70
tacone <tacone@gmail.com>
2021-03-29 18:26:33 +0200

    backend: basic production mode

--- backend/ormconfig.ts
+++ backend/ormconfig.ts
@@ -2,7 +2,12 @@ export default {
   type: "postgres",
   url: process.env.DATABASE_URL,
   synchronize: false,
-  entities: [__dirname + "/src/models/*.ts"],
+  entities: [
+    __dirname + "/src/models/*.ts",
+    // for some reason even in production __dirname
+    // is `/app` and not `/app/dist`
+    __dirname + "/dist/src/models/*.js",
+  ],
   migrations: ["src/migrations/*.ts"],
   cli: {
     migrationsDir: "src/migrations",
