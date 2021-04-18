2eb6eda355b1fb4bc3af8f41349747174bd26ed3
tacone <tacone@gmail.com>
2021-04-19 00:36:59 +0200

    svelte: add felte, implement form & validation

--- svelte/src/lib/Field.svelte
+++ svelte/src/lib/Field.svelte
@@ -3,12 +3,13 @@
 
 	export let id;
 	export let label;
-	export let touched;
-	export let error;
+	// export let error;
 	export let name = '';
 
+	export let tag = 'input';
+
 	label = label || humanizeString(name);
-	id = `_survey_${name}`;
+	id = id || `_survey_${name}`;
 </script>
 
 <div class="form-group row">
@@ -16,11 +17,24 @@
 		{label}
 	</label>
 	<div class="col-sm-10">
-		<slot />
-		{#if touched && error}
+		{#if tag === 'input'}
+			<h1>
+				<input {id} type="text" class="form-control" {...$$props} />
+			</h1>
+		{:else if tag === 'select'}
+			<h2>
+				<select {id} class="form-control" {...$$props}><slot /></select>
+			</h2>
+		{:else if tag === 'textarea'}
+			<h3>
+				<textarea {id} class="form-control" {...$$props} />
+			</h3>
+		{/if}
+		<div data-felte-reporter-dom-for={name} class="error" />
+		<!-- {#if error}
 			<small id="emailHelp" class="form-text text-danger">
 				{error}
 			</small>
-		{/if}
+		{/if} -->
 	</div>
 </div>
--- svelte/src/lib/Survey.svelte
+++ svelte/src/lib/Survey.svelte
@@ -1,50 +1,51 @@
 <script>
-	import Form from '../svelte-forms/index';
-	import undot from '$utils/objects/undot';
+	import { createForm } from 'felte';
 	import Field from './Field.svelte';
-	import dot from '$utils/objects/dot';
+	import reporterDom from '@felte/reporter-dom';
+	import { composeValidators, isEmail, isRequired, mapValidators } from '$utils/validators';
 
-	let values;
+	const { form, data, errors, isValid, isSubmitting } = createForm({
+		validate: mapValidators({
+			name: isRequired,
+			email_address: composeValidators(isRequired, isEmail)
+		}),
 
-	// we can use undot to support dot notation in fieldnames
-	// and have nested data
-	$: data = undot(values);
+		extend: reporterDom({
+			single: true
+		}),
+
+		onSubmit: async (values) => {
+			alert(JSON.stringify(values));
+			/* call to an api */
+		}
+	});
+	// $: console.log('form', form);
+	// $: console.log($data);
+	// $: console.log('errors', $errors);
 </script>
 
-<div>
-	<Form bind:values>
-		<Field name="name.first" touched="true" error="vietato!">
-			<input type="text" name="name.first" class="form-control" />
-		</Field>
+<form use:form>
+	<Field name="name" />
+	<Field name="email_address" />
+	<Field name="age" />
+	<Field name="gender" tag="select">
+		<option value="">--- choose ---</option>
+		<option value="male">Male</option>
+		<option value="female">Female</option>
+	</Field>
+	<Field name="country" />
+	<Field name="experience_rating" tag="select">
+		<option value="">--- choose ---</option>
+		<option value="5">5 - Great!</option>
+		<option value="4">4 - Good</option>
+		<option value="3">3 - Average</option>
+		<option value="2">2 - Not so good</option>
+		<option value="1"> 1 - Let's pretend we don't know each other </option>
+	</Field>
+	<Field name="suggested_improvements" tag="textarea" />
 
-		<div>
-			<button
-				type="button"
-				on:click={() => {
-					values = { ciaone: 'reset' };
-				}}>reset</button
-			>
+	<input type="submit" value="Sign in" disabled={!$isValid  || $isSubmitting} />
+</form>
 
-			<button
-				type="button"
-				on:click={() => {
-					console.log('values', values);
-				}}>go</button
-			>
-		</div>
-	</Form>
-
-	<pre>
-		{JSON.stringify(data, undefined, 4)}
-	</pre>
-
-	<pre>
-		{JSON.stringify(dot(data), undefined, 4)}
-	</pre>
-</div>
-
-<style lang="scss" ✂prettier:content>
-	pre {
-		margin-top: 20px;
-	}
-</style>
+<pre>{JSON.stringify($data, undefined, 2)}</pre>
+<pre>{JSON.stringify($errors, undefined, 2)}</pre>
