165859323017f029f916307382412d007dac09ec
tacone <tacone@gmail.com>
2021-04-19 23:23:10 +0200

    add strapi

---
+++ strapi/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '2e44dd89cc1a50054119ad63bbcd3746'),
+    },
+  },
+});
---
+++ strapi/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '31894ab0-fde2-4390-8467-0eaeb0640d13'
+};
