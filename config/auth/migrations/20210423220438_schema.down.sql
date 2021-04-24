e49a96c7bbce4483ce8b0d50bb0fbd88b595e5f5
tacone <tacone@gmail.com>
2021-04-24 21:39:21 +0200

    Add Netlify GoTrue auth
    https://github.com/netlify/gotrue
    
    - you need to run migrations manually with `gotrue migrate`
    - migrations only run on `auth` schema
    - largely untested

---
+++ config/auth/migrations/20210423220438_schema.down.sql
@@ -0,0 +1,13 @@
+DROP TABLE auth.audit_log_entries;
+
+DROP TABLE auth.identities;
+
+DROP TABLE auth.instances;
+
+DROP TABLE auth.refresh_tokens;
+
+DROP TABLE auth.schema_migrations;
+
+DROP TABLE auth.templates;
+
+DROP TABLE auth.users;
