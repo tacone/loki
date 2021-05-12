a0c6540c37a4382a18ad11a55e22498eaf3f3a6c
tacone <tacone@gmail.com>
2021-05-12 21:28:48 +0200

    backend: label migration, comment schema fields

--- backend/generated/schema.sql
+++ backend/generated/schema.sql
@@ -39,6 +39,83 @@ CREATE TABLE public.submissions (
 
 
 --
+-- Name: TABLE submissions; Type: COMMENT; Schema: public; Owner: -
+--
+
+COMMENT ON TABLE public.submissions IS 'A person''s submission to the survey';
+
+
+--
+-- Name: COLUMN submissions.id; Type: COMMENT; Schema: public; Owner: -
+--
+
+COMMENT ON COLUMN public.submissions.id IS 'An unique incremental ID';
+
+
+--
+-- Name: COLUMN submissions.name; Type: COMMENT; Schema: public; Owner: -
+--
+
+COMMENT ON COLUMN public.submissions.name IS 'The person full name';
+
+
+--
+-- Name: COLUMN submissions.email_address; Type: COMMENT; Schema: public; Owner: -
+--
+
+COMMENT ON COLUMN public.submissions.email_address IS 'The person unverified email address';
+
+
+--
+-- Name: COLUMN submissions.age; Type: COMMENT; Schema: public; Owner: -
+--
+
+COMMENT ON COLUMN public.submissions.age IS 'The person age';
+
+
+--
+-- Name: COLUMN submissions.gender; Type: COMMENT; Schema: public; Owner: -
+--
+
+COMMENT ON COLUMN public.submissions.gender IS 'The person gender';
+
+
+--
+-- Name: COLUMN submissions.country; Type: COMMENT; Schema: public; Owner: -
+--
+
+COMMENT ON COLUMN public.submissions.country IS 'The country of origin';
+
+
+--
+-- Name: COLUMN submissions.experience_rating; Type: COMMENT; Schema: public; Owner: -
+--
+
+COMMENT ON COLUMN public.submissions.experience_rating IS 'A rating from 1 to 5';
+
+
+--
+-- Name: COLUMN submissions.suggested_improvements; Type: COMMENT; Schema: public; Owner: -
+--
+
+COMMENT ON COLUMN public.submissions.suggested_improvements IS 'A short opinion';
+
+
+--
+-- Name: COLUMN submissions.referrer; Type: COMMENT; Schema: public; Owner: -
+--
+
+COMMENT ON COLUMN public.submissions.referrer IS 'The URL of the referring page';
+
+
+--
+-- Name: COLUMN submissions.created_at; Type: COMMENT; Schema: public; Owner: -
+--
+
+COMMENT ON COLUMN public.submissions.created_at IS 'The date/time of the submission';
+
+
+--
 -- Name: submissions_id_seq; Type: SEQUENCE; Schema: public; Owner: -
 --
 
---
+++ backend/migrations/committed/000001-create-submissions-table.sql
@@ -0,0 +1,31 @@
+--! Previous: -
+--! Hash: sha1:2420a9a32121ea4d394a77f40941e9a264256f22
+
+DROP TABLE IF EXISTS submissions;
+
+CREATE TABLE submissions (
+	id serial NOT NULL,
+	"name" varchar NOT NULL,
+	email_address varchar NOT NULL,
+	age int4 NULL,
+	gender varchar NULL,
+	country varchar NULL,
+	experience_rating int4 NULL,
+	suggested_improvements varchar NULL,
+	referrer varchar NULL,
+	created_at date NOT NULL DEFAULT CURRENT_TIMESTAMP,
+	CONSTRAINT "PK_10b3be95b8b2fb1e482e07d706b" PRIMARY KEY (id)
+);
+
+COMMENT ON TABLE public.submissions IS 'A person''s submission to the survey';
+
+COMMENT ON COLUMN public.submissions.id IS 'An unique incremental ID';
+COMMENT ON COLUMN public.submissions.name IS 'The person full name';
+COMMENT ON COLUMN public.submissions.email_address IS 'The person unverified email address';
+COMMENT ON COLUMN public.submissions.age IS 'The person age';
+COMMENT ON COLUMN public.submissions.gender IS 'The person gender';
+COMMENT ON COLUMN public.submissions.country IS 'The country of origin';
+COMMENT ON COLUMN public.submissions.experience_rating IS 'A rating from 1 to 5';
+COMMENT ON COLUMN public.submissions.suggested_improvements IS 'A short opinion';
+COMMENT ON COLUMN public.submissions.referrer IS 'The URL of the referring page';
+COMMENT ON COLUMN public.submissions.created_at IS 'The date/time of the submission';
